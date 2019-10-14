2d57b8c8567e43345f2001e1d63e660561e25650
Tim <tbartonclark@gmail.com>
2019-10-13 18:40:04 -0700

    add mv and rm functions
    - add validateRange function
    - rename rev and slew to rv and slw

--- scripture.lua
+++ scripture.lua
@@ -6,6 +6,7 @@
 
 sequence = {}
 index = 0
+queue = {}
 
 function init()
   rndm()
@@ -13,11 +14,16 @@ function init()
   input[1].change = handleChangeClock
   input[2].mode('change', 1, 0.05, 'rising')
   input[2].change = handleChangeInput2
-  print('sequencer script loaded')
+  print('Scripture loaded')
 end
 
 function handleChangeClock(state)
+  if index == 0 and #queue > 0 then
+    queue[1]()
+    table.remove(queue, 1)
+  end
   local step = sequence[index + 1]
+  print(index + 1)
   output[1].slew = step.slew
   output[1].volts = n2v(step.note)
   output[2].action = step.eg
@@ -25,8 +31,31 @@ function handleChangeClock(state)
   index = ((index + 1) % #sequence)
 end
 
-function handleChangeInput2(state)
-  rndm()
+function handleChangeInput2(state) rndm() end
+
+function validateRange(first, last)
+  if last < first or first < 1 or last > #sequence then
+    print('invalid params') return false
+  end
+  return true
+end
+
+function copyStep(step)
+  local newStep = {}
+  for key,value in pairs(step) do newStep[key] = value end
+  return newStep
+end
+
+function parseArgs(args)
+  local first, last, value = 1, #sequence, 1
+  if #args >= 3 then
+    first, last, value = args[1], args[2], args[3]
+  elseif #args == 2 then
+    first, last, value = args[1], #sequence, args[2]
+  elseif #args == 1 then
+    value = args[1]
+  end
+  return {['first'] = first, ['last'] = last, ['value'] = value }
 end
 
 -- user functions to invoke from druid below --
@@ -46,16 +75,19 @@ function rndm()
 end
 
 -- change octaves, default 1 octave up entire sequence
-function oct(mult, first, last)
-  mult = mult or 1
-  tp(12 * mult, first, last)
+function oct(...)
+  local args = parseArgs({...})
+  tp(args['first'], args['last'], 12*args['value'])
 end
 
 -- transpose, default 1 semitone
-function tp(value, first, last)
-  first = first or 1
-  last = last or #sequence
-  if last < first then print('invalid params') return end
+function tp(...)
+  local args = parseArgs({...})
+  local first = args['first']
+  local last = args['last']
+  local value = args['value']
+  print(first .. ' ' .. last .. ' ' .. value)
+  if not validateRange(first, last) then return end
   for i=first,last do
     sequence[i].note = sequence[i].note + value
   end
@@ -63,19 +95,17 @@ function tp(value, first, last)
 end
 
 -- reverse entire sequence or subsequence if first and last provided
-function rev(first, last)
+function rv(first, last)
   first = first or 1
   last = last or #sequence
-  if last < first then print('invalid params') return end
+  if not validateRange(first, last) then return end
   local reversed = {}
   local idx = first
   for i=last,first,-1 do
     reversed[idx] = sequence[i]
     idx = idx + 1
   end
-  for i=first,last do
-    sequence[i] = reversed[i]
-  end
+  for i=first,last do sequence[i] = reversed[i] end
   show()
 end
 
@@ -83,23 +113,64 @@ end
 function cp(first, last)
   first = first or 1
   last = last or #sequence
-  if last < first then print('invalid params') return end
+  if not validateRange(first, last) then return end
   for i=first,last do
-    local origStep = sequence[i]
-    local newStep = {note = origStep.note, slew = origStep.slew, eg = origStep.eg}
-    table.insert(sequence, newStep)
+    table.insert(sequence, copyStep(sequence[i]))
   end
   show()
 end
 
--- slew step with value
-function slew(step, value)
-  sequence[step].slew = value
+function setValueOrRange(args, param)
+  if #args >= 3 then
+    for i=args[1],args[2] do sequence[i][param] = args[3] end
+  elseif #args == 2 then
+    sequence[args[1]][param] = args[2]
+  elseif #args == 1 then
+    for i=1,#sequence do sequence[i][param] = args[1] end
+  end
 end
 
--- change step envelope - value is ASL function
-function eg(step, value)
-  sequence[step].eg = value
+-- slew - if only 2 args, slew step at arg1 with value at arg2 - else slew range
+function slw(...) setValueOrRange({...}, 'slew') end
+
+-- change step envelope, value is ASL function - if only 2 args, slew step at arg1 with value at arg2 - else slew range
+function eg(...) setValueOrRange({...}, 'eg') end
+
+-- move step to different position
+function mv(step, pos)
+--  local fn = function()
+    step = step or 1
+    pos = pos or #sequence
+    if sequence[step] == nil or sequence[pos] == nil then
+      print('invalid params') return
+    end
+    local stepToMove = sequence[step]
+    table.remove(sequence, step)
+    table.insert(sequence, pos, stepToMove)
+    show()
+--  end
+--  table.insert(queue, fn)
+end
+
+-- remove range of notes
+function rm(first, last)
+--  local fn = function()
+    last = last or #sequence
+    if not validateRange(first, last) then return end
+    local newSeq = {}
+    if first > 1 then
+      for i=1,first - 1 do
+        table.insert(newSeq, sequence[i])
+      end
+    end
+    for i=last+1,#sequence do
+      table.insert(newSeq, sequence[i])
+    end
+    if index > #newSeq then index = 0 end
+    sequence = newSeq
+    show()
+--  end
+--  table.insert(queue, fn)
 end
 
 -- print sequence notes
